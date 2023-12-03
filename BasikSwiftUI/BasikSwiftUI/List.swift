98b1be588df562c5bc40e97b1444aed6c24b4731
MrCronkite <vladshima99@gmail.com>
2023-12-03 15:01:06 +0300

    gesture

--- BasikSwiftUI/BasikSwiftUI/List.swift
+++ BasikSwiftUI/BasikSwiftUI/List.swift
@@ -2,8 +2,36 @@
 
 import SwiftUI
 
+struct Restaurant: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+struct RestaurantRow: View {
+    var restaurant: Restaurant
+    
+    var body: some View {
+        Text("Come and eat at \(restaurant.name)")
+    }
+}
+
 struct ListContentView: View {
     var body: some View {
+        let restorants: [Restaurant] = [
+            .init(name: "First restaurant"),
+            .init(name: "Second restaurant"),
+            .init(name: "Third restaurant")
+        ]
         
+        List(restorants) { rest in
+            Section(header: Text("Important tsks")) {
+                RestaurantRow(restaurant: rest)
+                RestaurantRow(restaurant: rest)
+            }
+        }
     }
+}
+
+#Preview {
+    ListContentView()
 }
