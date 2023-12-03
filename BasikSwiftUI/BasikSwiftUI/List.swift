38256bcdc323c4d39a2ccd9079039aea6a9908dd
MrCronkite <vladshima99@gmail.com>
2023-12-03 17:04:30 +0300

    gesture

--- BasikSwiftUI/BasikSwiftUI/List.swift
+++ BasikSwiftUI/BasikSwiftUI/List.swift
@@ -17,16 +17,35 @@ struct RestaurantRow: View {
 
 struct ListContentView: View {
     var body: some View {
-        let restorants: [Restaurant] = [
+        var restorants: [Restaurant] = [
             .init(name: "First restaurant"),
             .init(name: "Second restaurant"),
             .init(name: "Third restaurant")
         ]
         
-        List(restorants) { rest in
-            Section(header: Text("Important tsks")) {
-                RestaurantRow(restaurant: rest)
-                RestaurantRow(restaurant: rest)
+        NavigationView {
+            List {
+                Section(header: Text("Important tsks")) {
+                    ForEach(restorants) { restorant in
+                        RestaurantRow(restaurant: restorant)
+                        RestaurantRow(restaurant: restorant)
+                    }
+                    .onDelete { restorants.remove(atOffsets: $0)}
+                    .onMove { restorants.move(fromOffsets: $0, toOffset: $1)}
+                }
+            }
+            .navigationTitle("Welcome")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Help") {
+                        print("Help tapped!")
+                    }
+                }
             }
         }
     }
